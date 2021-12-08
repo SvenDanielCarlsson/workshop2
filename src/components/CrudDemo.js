cfb3ce68c626d38bb0a3a808ef577152be242616
SvenDanielCarlsson <sven.daniel.carlsson@gmail.com>
2021-12-08 16:40:06 +0100

    API implemented, ignore dbMOCK

--- src/components/CrudDemo.js
+++ src/components/CrudDemo.js
@@ -1,67 +1,89 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const CrudDemo = () => {
     
-
-    //  States
-    const allPersons = [
-        {id: 1, name: "Daniel Storsten", email: "fake@mail.facom"},
-        {id: 2, name: "Gustav Tros", email: "trosn@gunnar.facom"},
-        {id: 3, name: "Mats Karlsson", email: "mats@karlsson.facom"}
-    ];
-    const [persons, setPersons] = useState(allPersons);
+    const [persons, setPersons] = useState([]);
     const [person, setPerson] = useState({id: 0, name: "", email: ""});
 
+    const API_URL = "https://localhost:44342/People";
+ 
+    
+    useEffect(() => {
+        
+        const getApi = async () => {
+            const response = await axios.get(API_URL);
+            if(response.status === 200){
+                console.log("Get API successfull");
+                console.log(response.data);
+                setPersons(response.data);
+            }else{
+                console.log("Get API failed")
+            }
+        };
+
+        getApi();
+
+    }, []); // empty [] means it will only run once
+
+    
     const TableHeader = () => {
         return(
             <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
+            <tr>
+                <th>Id</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Action</th>
+            </tr>
+        </thead>
         );
     };
-
-    const TableRow = () => {
-        return (
-            <tbody>
-                <tr>
-                    <td>info</td>
-                    <td>info</td>
-                    <td>info</td>
-                    <td><TableAction /></td>
-                </tr>
-            </tbody>
-
-        );
-    };
-
+    
     const TableAction = () => {
         return(
             <ul className="nav">
-                <li><input type="button" className="details" name="details" value="Details" /></li>
-                <li><input type="button" className="bg-dark" name="delete" value="Delete" /></li>
-                <li><input type="button" className="bg-light" name="edit" value="Edit" /></li>
+                <li><input type="button" className="btn btn-primary" name="details" value="Details" /></li>
+                <li><input type="button" className="btn btn-danger" name="delete" value="Delete" /></li>
+                <li><input type="button" className="btn btn-warning" name="edit" value="Edit" /></li>
             </ul>
         );
-    }
+    };
+
+    const TableRow = (props) => {
+    return <tbody>
+            {
+                props.people.map(showPeople=>{
+
+                    const cyclePeople=<tr key={showPeople.id}>
+                        <td>{showPeople.id}</td>
+                        <td>{showPeople.firstName}</td>
+                        <td>{showPeople.lastName}</td>
+                        <td>{showPeople.email}</td>
+                        <td><TableAction /></td>
+                    </tr>
+
+                    return cyclePeople;
+                })
+            }
+        </tbody>
+    };
 
 
+    const Table = () => {
+        return <div className="container">
+                <br/>
+                <h3>List Of People</h3>
+                <table className="table">
+                    <TableHeader/>
+                    <TableRow people={persons} />
+                </table>
+            </div>
+    };
 
-
-    //  Return for CrudDemo, aka html presentation
-    return(
-        <div className="container">
-            <h2>THIS IS CRUUUUUUD!</h2>
-            <table className="table">
-                <TableHeader/>
-                <TableRow />
-            </table>
-        </div>
-        );
+    
+    return(Table());    //  Return for CrudDemo
 };
 
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import DemoRouter from './components/DemoRouter';
 
+
 ReactDOM.render(
-  <div className="container">
-    <h2>Greetings, here things are rendered</h2>
-    <DemoRouter />
-  </div>,
+    <DemoRouter />,
   document.getElementById('root')
 );
+
+// ugly using <div className="container"> around renderobjects?
